5a76506fae6a9d17060faf17a20c0bded07a2cb5
PrivanzaWisnu <162411668+PrivanzaWisnu@users.noreply.github.com>
2025-12-24 14:31:29 +0700

    Data Updates2.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <title>@yield('title')</title>
     <link rel="icon" href="https://privanzaportfolio.vercel.app/img/portfolio_icon.jpg">
 
-    <link href="{{ asset('bootstrap-5.3.8/dist/css/bootstrap.min.css') }}" rel="stylesheet">
-    <script src="{{ asset('bootstrap-5.3.8/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
   <style>
     body { background: linear-gradient(180deg,#0b0f15,#07101a); color:#e6f7ff; }
